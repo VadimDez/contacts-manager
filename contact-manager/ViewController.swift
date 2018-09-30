56625eae8f5f220e769e866069766918f7131d67
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2018-09-30 21:22:04 +0200

    init

---
+++ contact-manager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  contact-manager
+//
+//  Created by Vadim Dez on 30.09.18.
+//  Copyright © 2018 Vadim Dez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
